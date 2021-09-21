3e929e9fc93d35c84b55f7a2d9d0bf978232646d
gucio321 <gucio321@protonmail.com>
2021-09-21 21:42:57 +0200

    add possibility to use markdown

--- conf.py
+++ conf.py
@@ -28,7 +28,8 @@ author = '3Bp'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        "sphinx.ext.githubpages"
+        "sphinx.ext.githubpages",
+        "myst_parser"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
