a6a6f6779ae843ac388673c89c3f96af47249b5e
gucio321 <gucio321@protonmail.com>
2021-09-21 20:26:24 +0200

    put build stuff in docs folder

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ language = 'pl'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['docs', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
